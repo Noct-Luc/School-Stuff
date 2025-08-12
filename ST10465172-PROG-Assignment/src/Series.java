acb5933991ab32d6f5b23be2791ae1bad2354a29
J Calvert <zanoftheworld@gmail.com>
2025-08-12 14:57:08 +0000

    Series.java

--- ST10465172-PROG-Assignment/src/Series.java
+++ ST10465172-PROG-Assignment/src/Series.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
+
+
 public class Series extends SeriesModel{
     public Series( String SeriesId, String SeriesName, String SeriesAge, String SeriesNumberOfEpisodes){
         super(SeriesId,  SeriesName, SeriesAge, SeriesNumberOfEpisodes);
     }
-    public String captureSeries (){
-       System.out.println();
+    public static String captureSeries(){
+        Scanner scanner = new Scanner(System.in);
+        String id;
+        String name;
+        String age;
+        String episodeNum = "";
 
-
-
-        return "";
-    }
+        System.out.println("Enter the series ID");
+        id = scanner.nextLine();
+        System.out.println("Enter the series name");
+        name= scanner.nextLine();
+        System.out.println("Enter the series age restriction");
+        age = scanner.nextLine();
+        System.out.println("Enter the series number of episodeNum");
+        episodeNum = scanner.nextLine();
+        Series newSeries = new Series( id, name, age,episodeNum );
+        seriesList.add(newSeries);
+        System.out.println("Series successfully stored.");
+        return id;
+    };
 
 
 }
